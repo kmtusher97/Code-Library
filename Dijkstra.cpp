06d92d194f7a7e09ab5efdea078025f947a81293
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2019-01-31 06:26:17 +0600

    Dijkstra

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -4,22 +4,22 @@ dist[s] = 0;          //s = source
 priority_queue< pii, vector<pii>, greater<pii> > pq;
 pq.push({0, s});
 
-while( !pq.empty() )   
+while( !pq.empty() )
 {
-    pii front = pq.top();
-    pq.pop(); 
-    int d = front.first;
-    int u = front.second;
-    if( d > dist[u] )
-        continue;     // optimization
+  pii front = pq.top();
+  pq.pop();
+  int d = front.first;
+  int u = front.second;
+  if( d > dist[u] )
+    continue;     // optimization
 
-    for(int i = 0; i < G[u].size(); i++)
+  for(int i = 0; i < G[u].size(); i++)
+  {
+    pii v = G[u][j];
+    if( dist[u] + v.second < dist[v.first] )
     {
-        pii v = G[u][j]; 
-        if( dist[u] + v.second < dist[v.first] )
-        {
-            dist[v.first] = dist[u] + v.second; // relax 
-            pq.push({dist[v.first], v.first});
-        }
+      dist[v.first] = dist[u] + v.second; // relax
+      pq.push({dist[v.first], v.first});
     }
-} 
+  }
+}
