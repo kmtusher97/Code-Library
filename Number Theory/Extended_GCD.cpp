9923eba941b88b86b5d65dcd66512d102ae57b18
kmtusher97@gmail <kmtusher97@gmail.com>
2019-09-26 23:45:46 +0600

    Extended Euclidean Algorithm

--- Number Theory/Extended_GCD.cpp
+++ Number Theory/Extended_GCD.cpp
@@ -1,25 +1,31 @@
-int extended_gcd( int a, int b, int& x, int &y )
-{
-  if( a == 0 )
-  {
-    x = 0, y = 1;
-    return b;
-  }
-  int x1, y1;
-  int gcd = extended_gcd( b % a, a, x1, y1 );
+/**
+ * Extended Euclidean Algorithm
+ * a⋅x + b⋅y = gcd(a, b)
+ */
+int extended_gcd( int a, int b, int& x, int &y ) {
+    if( a == 0 ) {
+        x = 0, y = 1;
+        return b;
+    }
+    int x1, y1;
+    int gcd = extended_gcd( b % a, a, x1, y1 );
 
-  x = y1 - (b / a) * x1;
-  y = x1;
-  return gcd;
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return gcd;
 }
 
-int mod_inverse( int a, int m ) //ax + my = 1
-{
-  int x, y;
-  int gcd = extended_gcd( a, m, x, y );
-  if( gcd != 1 )
-  {
-    return -1;    // modular inverse doesn't exits
-  }
-  return (x % m + m) % m;
+/**
+ * Modular Inverse using Extended Euclidean algorithm
+ * a⋅x + m⋅y = 1
+ * a⋅x ≡ 1 (mod m)
+ */
+int mod_inverse( int a, int m ) {
+    int x, y;
+    int gcd = extended_gcd( a, m, x, y );
+    if( gcd != 1 ) {
+        /**modular inverse doesn't exits*/
+        return -1;
+    }
+    return (x % m + m) % m;
 }
