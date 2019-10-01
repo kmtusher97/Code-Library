e99e8936625a84ffaa3ec1b432b1aa2178b0e9de
kmtusher97@gmail <kmtusher97@gmail.com>
2019-10-02 00:15:45 +0600

    Fenwick Tree

--- Data Structure/BIT.cpp
+++ Data Structure/BIT.cpp
@@ -1,45 +1,22 @@
-ll tree[MX_N];
-
-void update( int id, ll vlu )
-{
-  for(; id <= n; tree[id] += vlu, id += (id & -id));
-}
-
-ll query( int id )
-{
-  ll ret = 0;
-  for(; id > 0; ret += tree[id], id -= (id & -id));
-  return ret;
-}
-
-/*-------------------------------------------------------*/
-
-template <class T> class Fenwick_tree
-{
-private:
-  int n;
-  vector<T> tree;
-
-public:
-  Fenwick_tree()
-  {
-    n = 0, tree.clear();
-  }
-  Fenwick_tree( int _n )
-  {
-    n = _n;
-    tree = vector<T> (n + 2, 0);
-  }
-  void update( int id, T vlu )
-  {
-   for(; id <= n; tree[id] += vlu, id += (id & -id));
-  }
-  T query( int id )
-  {
-    T ret = 0;
-    for(; id > 0; ret += tree[id], id -= (id & -id));
-    return ret;
-  }
+/**
+ * Fenwick Tree 
+ */
+struct BIT {
+	int n;
+	vector<ll> tt;
+	BIT() {}
+	BIT(int n) : n(n) {
+		tt.resize(n + 10, 0);
+	}
+	void update( int id, ll vlu ) {
+		if( id == 0 ) return;
+		for(; id <= n; tt[id] += vlu, id += (id & -id));
+	}
+	ll query( int id ) {
+		if( id < 1 ) return 0;
+		ll ret = 0;
+		for(; id > 0; ret += tt[id], id -= (id & -id));
+		return ret;
+	}
 };
-
 
